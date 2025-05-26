657ce5a6dbbb0f66548c666fa1e2afd905f71161
nelsonholanda <nelsonholanda@hotmail.com>
2025-05-26 14:35:16 -0300

    update node

--- deploy-front.sh
+++ deploy-front.sh
@@ -1,6 +1,7 @@
-API_ENDPOINT=http://d6dh6hbhk150r.cloudfront.net
+API_ENDPOINT=http://localhost:3001
 REACT_APP_API_URL=$API_ENDPOINT SKIP_PREFLIGHT_CHECK=true npm run build --prefix client
-echo '>> Deploy assets S3'
-aws s3 sync client/build s3://b1a-cdn --exclude "index.html"
-echo '>> Deploy index.html'
-aws s3 sync client/build s3://b1a-cdn --exclude "*" --include "index.html"
+echo '>> Fazendo deploy dos assets'
+aws s3 sync client/build s3://b1a-cdn/ --exclude "index.html"
+
+echo '>> Fazendo deploy do index.html'
+aws s3 sync client/build s3://b1a-cdn/ --exclude "*" --include "index.html"
